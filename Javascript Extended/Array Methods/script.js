51af374acc85aa58de23eb18699540bcb90c5918
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-09-22 14:58:06 +0200

    Array Functions #2 - finished files

--- Javascript Extended/Array Methods/script.js
+++ Javascript Extended/Array Methods/script.js
@@ -14,15 +14,35 @@ const productNames = products.map((product) => {
     return product.name;
 })
 
-const foundItem = products.find((product) => {
+const foundProduct = products.find((product) => {
     return product.name == "Book";
 })
 
-console.log(products)
-console.log(filteredProducts)
-console.log(productNames)
-console.log(foundItem)
+const hasInexpensiveProducts = products.some((product) => {
+    return product.price <= 100;
+})
+
+const hasInexpensiveProductsAll = products.every((product) => {
+    return product.price <= 100;
+})
+
+const total = products.reduce((currentTotal, product) => {
+    return product.price + currentTotal;
+}, 0)
+
+console.log(products);
+console.log(filteredProducts);
+console.log(productNames);
+console.log(foundProduct);
+console.log(hasInexpensiveProducts);
+console.log(hasInexpensiveProductsAll);
+console.log(total);
 
 products.forEach((product) => {
     console.log(`${product.name} - ${product.price}$`);
 })
+
+const numbers = [1, 2, 3, 4, 5];
+const includesThree = numbers.includes(3);
+
+console.log(includesThree);
