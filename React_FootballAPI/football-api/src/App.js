0c2012c5cae6893d610710d3e4b118756172922c
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-15 21:51:49 +0100

    Made design changes for the tream info component

--- React_FootballAPI/football-api/src/App.js
+++ React_FootballAPI/football-api/src/App.js
@@ -4,9 +4,14 @@ import TeamInfo from './components/TeamInfo';
 function App() {
   return (
     <div className="App">
-      <TeamInfo teamPlace="1" teamName="Bayern" teamIcon="https://i.imgur.com/jJEsJrj.png"
-        teamPoints="28" teamGamesPlayed="10" teamGoalsScored="30" teamGoalsConceded="7"
-        teamWins="9" teamDraws="1" teamLoses="0" />
+      <TeamInfo teamPlace="1" teamName="Bayern" teamIcon="https://i.imgur.com/jJEsJrj.png"  teamPoints="28" teamGamesPlayed="10"
+        teamGoalsScored="30" teamGoalsConceded="7" teamWins="9" teamDraws="1" teamLoses="0" />
+      <TeamInfo teamPlace="2" teamName="Bayern" teamIcon="https://i.imgur.com/jJEsJrj.png" teamPoints="28" teamGamesPlayed="10"
+        teamGoalsScored="30" teamGoalsConceded="7" teamWins="9" teamDraws="1" teamLoses="0" />
+      <TeamInfo teamPlace="3" teamName="Bayern" teamIcon="https://i.imgur.com/jJEsJrj.png" teamPoints="28" teamGamesPlayed="10"
+        teamGoalsScored="30" teamGoalsConceded="7" teamWins="9" teamDraws="1" teamLoses="0" />
+      <TeamInfo teamPlace="4" teamName="Bayern" teamIcon="https://i.imgur.com/jJEsJrj.png" teamPoints="28" teamGamesPlayed="10"
+        teamGoalsScored="30" teamGoalsConceded="7" teamWins="9" teamDraws="1" teamLoses="0" />
     </div>
   );
 }
