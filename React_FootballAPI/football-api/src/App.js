8e02fb3908e52eeb78aa94e3316db3798c8fa622
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-18 17:57:14 +0100

    Added components for recent and upcoming games and fetched data

--- React_FootballAPI/football-api/src/App.js
+++ React_FootballAPI/football-api/src/App.js
@@ -1,26 +1,69 @@
 import './App.css';
 import TeamInfo from './components/TeamInfo';
 import React, { useState, useEffect } from 'react';
+import UpcomingGames from './components/UpcomingGames.module';
+import RecentGames from './components/RecentGames.module';
 
 function App() {
-  const [data, setData] = useState([])
+  const [teamsData, setTeamsData] = useState([]);
+  const [gamesData, setGamesData] = useState([]);
 
-  const loadData = () => {
+  const currentTime = new Date();
+  let upcomingGamesArray = [];
+  let recentGamesArray = [];
+  let amountOfUpcomingGames = 4;
+  let amountOfRecentGames = 4;
+
+  gamesData.forEach(element => {
+    let matchDate = Date.parse(element.matchDateTime);
+    if (matchDate > currentTime) {
+      upcomingGamesArray.push(element);
+    } else {
+      recentGamesArray.push(element);
+    }
+  })
+
+  const loadTeamsData = () => {
     fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
       result.json().then((data) => {
-        setData(data)
+        setTeamsData(data);
+      })
+    })
+  }
+
+  const loadGamesData = () => {
+    fetch("https://api.openligadb.de/getmatchdata/bl1/2023").then((result) => {
+      result.json().then((data) => {
+        setGamesData(data);
       })
     })
   }
 
   useEffect(() => {
-    loadData();
+    loadTeamsData();
+  }, [])
+
+  useEffect(() => {
+    loadGamesData();
   }, [])
 
   return (
     <div className="App">
+      <div id="games-info">
+        <div className="upcoming-games-title">Upcoming Games</div>
+        {upcomingGamesArray.slice(0, amountOfUpcomingGames).map((element) =>
+          <UpcomingGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl} upcomingGameDate={element.matchDateTime}
+            team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
+        )}
+        <div className="recent-games-title">Recent Games</div>
+        {recentGamesArray.slice(-amountOfRecentGames).map((element) =>
+          <RecentGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl}
+            team1Goals={element.matchResults[1].pointsTeam1} team2Goals={element.matchResults[1].pointsTeam2}
+            team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
+        )}
+      </div>
       <div id="team-info">
-        {data.map((element, index) =>
+        {teamsData.map((element, index) =>
           <TeamInfo teamPlace={index + 1} teamName={element.teamName} teamIcon={element.teamIconUrl} teamPoints={element.points}
             teamGamesPlayed={element.matches} teamGoalsScored={element.goals} teamGoalsConceded={element.opponentGoals}
             teamWins={element.won} teamDraws={element.draw} teamLoses={element.lost} />)}
---
+++ React_FootballAPI/football-api/src/components/RecentGames.module.js
@@ -0,0 +1,20 @@
+import styles from "./RecentGames.module.css"
+
+export default function RecentGames(props) {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return (
+        <div className={styles.recentGamesContainer}>
+            <div className={styles.recentGamesTeam1}>
+                <div className={styles.recentGamesTeamName}>{props.team1TeamName}</div>
+                <img className={styles.recentGamesTeamImg} src={props.team1IconURL} />
+            </div>
+            <div className={styles.recentGamesTeam1Result}>{props.team1Goals}</div>
+
+            <div className={styles.recentGamesTeam2}>
+                <div className={styles.recentGamesTeamName}>{props.team2TeamName}</div>
+                <img className={styles.recentGamesTeamImg} src={props.team2IconURL} />
+            </div>
+            <div className={styles.recentGamesTeam2Result}>{props.team2Goals}</div>
+        </div>
+    )
+}
