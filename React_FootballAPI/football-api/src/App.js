6c4d4a37a7a044c93ba1599593bcd1718c82f9ab
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-19 22:42:13 +0100

    Added header and made design changes

--- React_FootballAPI/football-api/src/App.js
+++ React_FootballAPI/football-api/src/App.js
@@ -11,8 +11,8 @@ function App() {
   const currentTime = new Date();
   let upcomingGamesArray = [];
   let recentGamesArray = [];
-  let amountOfUpcomingGames = 4;
-  let amountOfRecentGames = 4;
+  let amountOfUpcomingGames = 2;
+  let amountOfRecentGames = 2;
 
   gamesData.forEach(element => {
     let matchDate = Date.parse(element.matchDateTime);
@@ -49,24 +49,31 @@ function App() {
 
   return (
     <div className="App">
-      <div id="games-info">
-        <div className="upcoming-games-title">Upcoming Games</div>
-        {upcomingGamesArray.slice(0, amountOfUpcomingGames).map((element) =>
-          <UpcomingGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl} upcomingGameDate={element.matchDateTime}
-            team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
-        )}
-        <div className="recent-games-title">Recent Games</div>
-        {recentGamesArray.slice(-amountOfRecentGames).map((element) =>
-          <RecentGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl}
-            team1Goals={element.matchResults[1].pointsTeam1} team2Goals={element.matchResults[1].pointsTeam2}
-            team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
-        )}
+      <div className="header">
+        <img src="./Allianz Arena.png" className="alliance-arena-img" />
+        <div className="title"><span className="title-part-1">Bundes</span><span className="title-part-2">liga 1</span></div>
       </div>
-      <div id="team-info">
-        {teamsData.map((element, index) =>
-          <TeamInfo teamPlace={index + 1} teamName={element.teamName} teamIcon={element.teamIconUrl} teamPoints={element.points}
-            teamGamesPlayed={element.matches} teamGoalsScored={element.goals} teamGoalsConceded={element.opponentGoals}
-            teamWins={element.won} teamDraws={element.draw} teamLoses={element.lost} />)}
+
+      <div className="container">
+        <div id="games-info">
+          <div className="upcoming-games-title">Upcoming Games</div>
+          {upcomingGamesArray.slice(0, amountOfUpcomingGames).map((element) =>
+            <UpcomingGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl} upcomingGameDate={element.matchDateTime}
+              team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
+          )}
+          <div className="recent-games-title">Recent Games</div>
+          {recentGamesArray.slice(-amountOfRecentGames).map((element) =>
+            <RecentGames team1TeamName={element.team1.teamName} team1IconURL={element.team1.teamIconUrl}
+              team1Goals={element.matchResults[1].pointsTeam1} team2Goals={element.matchResults[1].pointsTeam2}
+              team2TeamName={element.team2.teamName} team2IconURL={element.team2.teamIconUrl} />
+          )}
+        </div>
+        <div id="team-info">
+          {teamsData.map((element, index) =>
+            <TeamInfo teamPlace={index + 1} teamName={element.teamName} teamIcon={element.teamIconUrl} teamPoints={element.points}
+              teamGamesPlayed={element.matches} teamGoalsScored={element.goals} teamGoalsConceded={element.opponentGoals}
+              teamWins={element.won} teamDraws={element.draw} teamLoses={element.lost} />)}
+        </div>
       </div>
     </div>
   );
