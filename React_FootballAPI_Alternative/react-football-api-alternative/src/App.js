469697a538fb93fe027c9f6b7c473645f078a4f6
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-04-04 23:38:56 +0200

    Added TeamInfo Component and added Tailwind Design

--- React_FootballAPI_Alternative/react-football-api-alternative/src/App.js
+++ React_FootballAPI_Alternative/react-football-api-alternative/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TeamInfo from './components/TeamInfo';
 
 function App() {
+  const [teamsData, setTeamsData] = useState([]);
+
+  const loadTeamsData = () => {
+    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
+      result.json().then((data) => {
+        setTeamsData(data);
+      })
+    })
+  }
+
+  useEffect(() => {
+    loadTeamsData();
+  }, [])
+
   return (
-    <h1 className="text-10xl font-bold underline">
-      Hello world!
-    </h1>
+    <div id="team-info"> {
+      teamsData.map((element, index) =>
+        <TeamInfo teamPlace={index + 1} teamName={element.teamName} teamIconURL={element.teamIconUrl} teamPoints={element.points}
+          teamGamesPlayed={element.matches} teamGoalsScored={element.goals} teamGoalsConceded={element.opponentGoals}
+          teamWins={element.won} teamDraws={element.draw} teamLoses={element.lost} />)
+    }</div>
   );
 }
 
---
+++ React_FootballAPI_Alternative/react-football-api-alternative/src/components/TeamInfo.js
@@ -0,0 +1,18 @@
+export default function TeamInfo(props) {
+  return (
+    <div className="grid grid-cols-12 bg-teaminfo-default p-2 relative items-center">
+      <div className=" bg-gradient-to-r from-orange-500 to-amber-700 font-bold"><span>{props.teamPlace}</span></div>
+      <div className="flex content-start col-span-3 font-semibold">{props.teamName}</div>
+      <div className="flex items-center content-center col-span-2"><img className="max-w-20" alt={props.teamName + "-Logo"} src={props.teamIconURL} /></div>
+      <div className="font-semibold text-lg col-span-2"><span>{props.teamPoints}</span> {props.teamPoints === 1 ? "PT" : "PTS"}</div>
+      <div className="grid grid-cols-6 gap-14">
+        <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGamesPlayed}</span>&nbsp;Pld</div>
+        <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGoalsScored}</span>&nbsp;GS</div>
+        <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGoalsConceded}</span>&nbsp;GC</div>
+        <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamWins}</span>&nbsp;W</div>
+        <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamDraws}</span>&nbsp;D</div>
+        <div className="flex content-center items-center font-light" ><span className="font-bold">{props.teamLoses}</span>&nbsp;L</div>
+      </div>
+    </div>
+  )
+}
