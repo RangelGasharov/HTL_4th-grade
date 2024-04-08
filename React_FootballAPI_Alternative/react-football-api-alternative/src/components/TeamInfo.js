621ad76cad51f228422b86d3bbc28908c20ec8d1
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-04-08 23:34:20 +0200

    Added minor design changes to the teaminfo component

--- React_FootballAPI_Alternative/react-football-api-alternative/src/components/TeamInfo.js
+++ React_FootballAPI_Alternative/react-football-api-alternative/src/components/TeamInfo.js
@@ -1,11 +1,13 @@
 export default function TeamInfo(props) {
   return (
-    <div className="grid grid-cols-12 bg-teaminfo-default p-2 relative items-center">
-      <div className=" bg-gradient-to-r from-orange-500 to-amber-700 font-bold flex items-center content-center"><span>{props.teamPlace}</span></div>
-      <div className="flex content-start col-span-3 font-semibold">{props.teamName}</div>
-      <div className="flex items-center justify-center content-center col-span-2"><img className="max-w-20 max-h-20" alt={props.teamName + "-Logo"} src={props.teamIconURL} /></div>
-      <div className="font-semibold text-lg col-span-2"><span>{props.teamPoints}</span> {props.teamPoints === 1 ? "PT" : "PTS"}</div>
-      <div className="grid grid-cols-6 gap-14">
+    <div className="lg:text-lg sm:text-sm grid grid-cols-12 bg-teaminfo-default p-2 relative items-center sm:grid-rows-2">
+      <div className="sm:row-span-1">
+        <div className="font-bold flex items-center content-center col-span-1"><span>{props.teamPlace}</span></div>
+        <div className="flex content-start col-span-2 font-semibold">{props.teamName}</div>
+        <div className="flex items-center justify-center content-center col-span-2"><img className="max-w-20 max-h-10 sm:max-w-10 sm:max-h-15" alt={props.teamName + "-Logo"} src={props.teamIconURL} /></div>
+        <div className="font-semibold text-lg col-span-2"><span>{props.teamPoints}</span> {props.teamPoints === 1 ? "PT" : "PTS"}</div>
+      </div>
+      <div className="grid grid-cols-6 gap-14 sm:row-span-1">
         <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGamesPlayed}</span>&nbsp;Pld</div>
         <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGoalsScored}</span>&nbsp;GS</div>
         <div className="flex content-center items-center font-light"><span className="font-bold">{props.teamGoalsConceded}</span>&nbsp;GC</div>
